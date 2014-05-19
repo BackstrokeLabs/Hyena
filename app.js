c20ab7cd2c7490edc51eda4208de67df23002709
yesyayen <yesyayen@gmail.com>
2014-05-20 01:53:28 +0530

    private chat module

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 	
 server.listen(8000);
 
-usernames = [];
+userObj = {};   //object - key value pair. Username - key and socket is value
 
 app.get('/',function(req, res) {
 	res.sendfile('/index.html');
@@ -15,28 +15,64 @@ app.get('/',function(req, res) {
 
 io.sockets.on('connection', function(socket){
 
+	function sendUserList(){
+		io.sockets.emit('user list',Object.keys(userObj));   //send the keys of object userObj i.e. sending usernames 
+	}
+
 	socket.on('new user', function(data, callback) {
 	data=data.trim();
-	if(usernames.indexOf(data) != -1){
+	//if(usernames.indexOf(data) != -1){
+	if(data in userObj){
 			callback(false);
 	} else{
 		callback(true);
 		socket.username = data;
-		usernames.push(socket.username);	
-		io.sockets.emit('user list',usernames); 
+		userObj[socket.username] = socket;  //pushing socket value keeping username as the key.	
+		sendUserList(); 
 		io.sockets.emit('new message',{'message': socket.username+' has joined newly','username': '**info**'});
 		}
 	});
 
-	socket.on('send message', function(data) {
-		io.sockets.emit('new message',{'message': data,'username': socket.username});   //send to everyone including the same client
-		/*socket.broadcast.emit('new message',data);*/ //send to everyone excluding the same client
+	socket.on('send message', function(data,callback) {
+		msg = data.trim();
+		privateKeyword = msg.split(" ")[0];
+
+		if(privateKeyword === '/chat' || privateKeyword === '/c') {
+			privateUser = msg.split(" ")[1];
+
+			if(privateUser !== socket.username) {
+				if(privateUser in userObj) {
+					console.log('private message');
+					privateMsg = msg.substring(privateKeyword.length+privateUser.length+1).trim();
+					if( privateMsg.length > 0 ) {
+						userObj[privateUser].emit('new private message',{'message': privateMsg,'username': socket.username,'ownMessage': 'false'});  //sending message to the private user
+						socket.emit('new private message',{'message': privateMsg,'username': privateUser,'ownMessage': 'true'});	//sending message to the owner of the message, for marking whom he/she has sent the message
+					}
+					else {
+						console.log('message not presemt');
+						callback('Error! Enter a message to send.');
+					}
+				}
+				else {
+					console.log('user not presemt');
+					callback('Error! User Not present.');
+				}
+			}
+			else {
+				console.log('Error! Cannot send private message to oneself');
+				callback('Error! Cannot send private message to oneself');
+			}
+		}
+		else {
+			io.sockets.emit('new message',{'message': data,'username': socket.username});   //send to everyone including the same client
+			/*socket.broadcast.emit('new message',data);*/ //send to everyone excluding the same client
+		}
 	});
 
 	socket.on('disconnect', function(data) {
 		if(!socket.username) return;
-		usernames.splice(usernames.indexOf(socket.username),1);
-		io.sockets.emit('user list',usernames); 
+		delete userObj[socket.username];
+		sendUserList();
 		io.sockets.emit('new message',{'message': socket.username+' disconnected from room','username': '**info**'});
 	});
 
